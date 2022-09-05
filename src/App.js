fd8374737721711d3b4df4db70a4422cfe092900
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-09-05 11:47:07 -0300

    Agregando carpetas y links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Detalle from './pages/detalle/Detalle';
+import NotFound from './pages/notfound/Notfound';
+import Album from './components/album/Album';
+import Songs from './components/song/Songs';
+import Favoritos from './pages/favoritos/Favoritos';
+import verMas from './pages/verMas/verMas';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
-  );
+    <>
+       <Header titulo="Deezer" subtitulo="Musica"/>        
+        {/* <Container /> */}
+        <Switch>
+          <Route path="/" exact  >
+            <Home />
+          </Route>
+          <Route path="/Detalle" component={Detalle} />
+          <Route path="/Album" component={Album} />
+          <Route path="/Song" component={Songs}/>
+          <Route path="/Favoritos" component={Favoritos} />
+          <Route path="/verMas" component={verMas} />
+          <Route component={NotFound}/>
+        </Switch>
+      <Footer /> 
+    </>
+    );  
 }
 
 export default App;
