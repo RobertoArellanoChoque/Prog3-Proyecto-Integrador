00176e24db224d07607cd2285de87901688501ba
benjaap <89359576+benjaap@users.noreply.github.com>
2022-09-16 21:33:50 -0300

    continuacion favoritos

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function Card({pelicula}) {
-let  {poster_path, title, overview, id} = pelicula 
-
-const borrar = () => {
-  console.log(borrar)
-}
+function Card(props) {
+let  {poster_path, title, overview, id} = props.pelicula
 
   return (
-
     <div className="hijo">
         <div className="imagen-port">
             <a href={`/detallepelicula/id/${id}`}><img src={`https://image.tmdb.org/t/p/original${poster_path}`} alt={poster_path}/></a>
             <h3>{title}</h3>
             <p>{overview}</p>
-            <button onClick={borrar}>Borrar</button>
+            <button className="btn btn-primary" onClick={()=>{props.favorito(props.pelicula)}} >Favoritos</button>
             <button><Link to={`/detallepelicula/id/${id}`}>Detalle</Link></button>
             <button><Link to={`//id/${id}`}>Ver Más</Link></button>
         </div>
--- src/pages/DetallePelicula/DetallePelicula.js
+++ src/pages/DetallePelicula/DetallePelicula.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Card from '../../components/card/Card';
 
 class DetallePelicula extends Component {
 
@@ -7,37 +8,66 @@ class DetallePelicula extends Component {
         this.state = {
             id: this.props.match.params.id,
             detail: {},
+            pelicula:[],
             favoritos: []
         }
     }
 
     componentDidMount() {
-        console.log(this.state.favoritos)  
-        this.setState({favoritos: JSON.parse(localStorage.getItem('favoritos')) || []})
+        console.log(this.state.favoritos)
+        this.setState({ favoritos: JSON.parse(localStorage.getItem('favoritos')) || [] })
         const url = `https://api.themoviedb.org/3/movie/${this.state.id}?api_key=c0945689b0a582e110971301d6ea8be2`
         fetch(url)
             .then((res) => res.json())
             .then(data => {
                 console.log(data)
-                        return this.setState({
-                        detail: data
-                    })
+                return this.setState({
+                    detail: data
+                })
             })
             .catch(err => console.log(err))
+    }
+
+    handleFavoritos(card) {
+        if (this.state.favoritos.some(fav => card.id === fav.id)) {
+            this.setState({ favoritos: this.state.favoritos.filter(item => item.id !== card.id) }, () => {
+                localStorage.setItem('favoritos', JSON.stringify(this.state.favoritos))
+            })
+            console.log(this.state.favoritos.filter(item => item.id !== card.id))
+        } else {
+            this.setState({ favoritos: [...this.state.favoritos, card] }, () => {
+                localStorage.setItem('favoritos', JSON.stringify(this.state.favoritos))
+            })
+        }
     }
 
     render() {
         return (
             <>
                 <h1>{this.state.detail.title}</h1>
-                <img src = {`https://image.tmdb.org/t/p/original${this.state.detail.poster_path}`} alt = {this.state.detail.title}/>
+                <img src={`https://image.tmdb.org/t/p/original${this.state.detail.poster_path}`} alt={this.state.detail.title} />
                 <p>Rating: {this.state.detail.vote_average}</p>
                 <p>Fecha de estreno: {this.state.detail.release_date}</p>
                 <p>Duración: {this.state.detail.runtime}</p>
                 <p>Sinópsis: {this.state.detail.overview}</p>
                 <p>Género al que pertenece la película: </p>
-                <button onClick={()=>console.log("Favoritos")}>Agregar a Favoritos</button>
+                <button onClick={() => { this.handleFavoritos() }}>Agregar a Favoritos</button>
+                <div>
+                    {
+                        this.state.cargando === false ? (
+                            <p>Cargando</p>
+                        ) :
+                            this.state.pelicula.map(pelicula => (
+                                <Card
+                                    key={pelicula.id}
+                                    pelicula={pelicula}
+                                    favorito={(pelicula) => this.handleFavoritos(pelicula)}
+                                />)
+                            )
+                    }
+                </div>
             </>
+
         )
     }
 }
