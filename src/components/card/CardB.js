7d4a767915be81d72f6f9bccfad7c248b71ffdc4
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-09-18 04:50:49 -0300

    Arreglos en botones

--- src/components/card/CardB.js
+++ src/components/card/CardB.js
@@ -1,37 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 
-function CardB(props) {
+class CardB extends Component {
+
+  mostrarDetalle (e){
+    let hideText = document.getElementById('hideText')
+    hideText.classList.toggle('info-show')
+  }
+
+  render(){
 
   return (
     <div className="hijo">
       <div className="imagen-port">
-        {props.format === "person" ?
+        {this.props.format === "person" ?
           <>
-            <a href={`/detallepelicula/id/${props.id}`}><img src={`https://image.tmdb.org/t/p/original${props.perfil}`} alt="no hay foto" /></a>
-            <h3>{props.persona}</h3>
+            <a href={`/detallepelicula/id/${this.props.id}`}><img src={`https://image.tmdb.org/t/p/original${this.props.perfil}`} alt="no hay foto" /></a>
+            <h3>{this.props.persona}</h3>
           </>
           :
           <>
             {
-              props.imagen === "" || props.imagen === null ? <a href={`/detallepelicula/id/${props.id}`}><img src='./img/noFoto.jpg' alt="no hay foto" /></a>
+              this.props.imagen === "" || this.props.imagen === null ? <a href={`/detallepelicula/id/${this.props.id}`}><img src='./img/noFoto.jpg' alt="no hay foto" /></a>
                 :
-                <a href={`/detallepelicula/id/${props.id}`}><img src={`https://image.tmdb.org/t/p/original${props.imagen}`} alt={props.poster_path} /></a>
+                <a href={`/detallepelicula/id/${this.props.id}`}><img src={`https://image.tmdb.org/t/p/original${this.props.imagen}`} alt={this.props.poster_path} /></a>
             }
-            {props.format === "movie" ?
-              <h3>{props.title}</h3>
+            {this.props.format === "movie" ?
+              <h3>{this.props.title}</h3>
               :
-              <h3>{props.name}</h3>
+              <h3>{this.props.name}</h3>
             }
-            <p>{props.overview}</p>
-            <button><Link to={`/detallepelicula/id/${props.id}`}>Detalle</Link></button>
-            <button><Link to={`//id/${props.id}`}>Ver Más</Link></button>
+            <span className='info' id='hideText' >
+            <p  >{this.props.overview}</p>
+
+
+            </span>
+            <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
+            <button onClick={() => this.mostrarDetalle()} >Ver mas</button>
 
           </>
         }
       </div>
     </div>
   )
+      }
 }
 
 export default CardB
