1c35bf5790980d744a6a876bdaa0f95b4b61ba7c
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-09-18 16:43:00 -0300

    Eroores de choque

--- src/components/card/CardB.js
+++ src/components/card/CardB.js
@@ -41,54 +41,30 @@ this.state = {
               {this.props.format === "movie" ?
                 <h3>{this.props.title}</h3>
                 :
-<<<<<<< HEAD
-                <a href={`/detallepelicula/id/${this.props.id}`}><img src={`https://image.tmdb.org/t/p/original${this.props.imagen}`} alt={this.props.poster_path} /></a>
-            }
-            {this.props.format === "movie" ?
-              <h3>{this.props.title}</h3>
-              :
-              <h3>{this.props.name}</h3>
-            }
-            {this.state.verMas === true ?
-            <>
-             <spam className='info' id='hideText' >
-              <p  >{this.props.overview}</p>
-            </spam>
-            <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
-            <button id='hideTextButton' onClick={() => this.mostrarDetalle()} >Ver mas</button>
-    
-            </>
-            :
-            <>
-            <spam className='info-show' id='hideText' >
-              <p  >{this.props.overview}</p>
-            </spam>
-            <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
-            <button id='hideTextButton' onClick={() => this.ocultarDetalle()} >Ver menos</button>
-            
-            
-            </>
-            
-            
-            }
-
-          </>
-        }
-=======
                 <h3>{this.props.name}</h3>
               }
-              <span className='info' id='hideText' >
+              {this.state.verMas === true ?
+              <>
+               <span className='info' id='hideText' >
                 <p  >{this.props.overview}</p>
-
-
               </span>
               <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
               <button onClick={() => this.mostrarDetalle()} >Ver mas</button>
+              
+              </>
+            :
+            <>
+             <span className='info-show' id='hideText' >
+                <p  >{this.props.overview}</p>
+              </span>
+              <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
+              <button onClick={() => this.ocultarDetalle()} >Ver menos</button>
+            </>
+            }
 
             </>
           }
         </div>
->>>>>>> c199a2f1ef6b8d30edbca83275a140aee3306ef8
       </div>
     )
   }
