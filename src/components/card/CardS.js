76e34fbfe149300c9e1dcd88fa89c7ce2dc8d908
RobertoArellanoChoque <robertoarellanoch@gmail.com>
2022-09-19 12:38:50 -0300

    Correccion

---
+++ src/components/card/CardS.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class CardS extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      button: JSON.parse(localStorage.getItem('favoritos')).some((fav)=> fav === this.props),
+      verMas: true,
+    }
+  }
+
+  handleFavoritos(){
+    this.setState({button: !this.state.button}, ()=>{this.props.favoritos(this.props.serie)})
+  }
+
+  mostrarDetalle() {
+    this.setState({
+      verMas: false,
+    })
+  }
+  ocultarDetalle() {
+    this.setState({
+      verMas: true,
+    })
+  }
+
+  render() {
+    return (
+      <div className="hijo">
+        <div className="imagen-port">
+          <a href={`/detalleserie/id/${this.props.id}`}><img src={`https://image.tmdb.org/t/p/original${this.props.poster_path}`} alt={this.props.poster_path} /></a>
+          <h3>{this.props.name}</h3>
+          {this.state.verMas === true ?
+                <>
+                  <span className='info' id='hideText' >
+                    <p  >{this.props.overview}</p>
+                  </span>
+                  <button><Link to={`/detalleserie/id/${this.props.id}`}>Detalle</Link></button>
+                  <button onClick={() => this.mostrarDetalle()} >Ver mas</button>
+
+                </>
+                :
+                <>
+                  <span className='info-show' id='hideText' >
+                    <p  >{this.props.overview}</p>
+                  </span>
+                  <button><Link to={`/detallepelicula/id/${this.props.id}`}>Detalle</Link></button>
+                  <button onClick={() => this.ocultarDetalle()} >Ver menos</button>
+                </>
+              }
+
+
+          
+
+
+          <button className="btn btn-primary" onClick={() => this.handleFavoritos()}>{this.state.button ? 'X' :'⭐' }</button>
+          
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CardS
